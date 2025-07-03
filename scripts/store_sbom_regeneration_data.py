3b5d0a4ba9e52232ca13520364ab3e528679979c
haripate <haripate@redhat.com>
2025-07-03 11:26:28 -0500

    feat:Fix the ruff check.

--- scripts/store_sbom_regeneration_data.py
+++ scripts/store_sbom_regeneration_data.py
@@ -1,14 +1,17 @@
-import boto3
-import json
-import os
-import logging
 import argparse
-from botocore.exceptions import BotoCoreError, ClientError
-from mobster.cmd.generate.product import ReleaseData
-from mobster.release import Snapshot
+import json
+import logging
+import os
 from typing import Any
 
+import boto3
+from botocore.exceptions import BotoCoreError, ClientError
+
+from mobster.cmd.generate.product import ReleaseData
+from mobster.release import Snapshot
+
 LOGGER = logging.getLogger(__name__)
+
 
 class BucketClient:
     """
@@ -29,50 +32,59 @@ class BucketClient:
                 Bucket=self.bucket,
                 Key=key,
                 Body=json.dumps(data),
-                ContentType="application/json"
+                ContentType="application/json",
             )
             LOGGER.info(f"Successfully Stored the SBOM to s3://{self.bucket}/{key}")
-        except (BotoCoreError, ClientError) as e:
+        except (BotoCoreError, ClientError):
             LOGGER.error(f"Failed to store SBOM to s3://{self.bucket}/{key}")
             raise
-    
+
+
 def store_sbom_data(
     data: dict,
     uid: str,
     bucket: BucketClient,
     model_class: Any,
     prefix: str,
-    log_tag: str
+    log_tag: str,
 ) -> None:
     try:
         validated_data = model_class(**data)
         bucket.upload_sboms(validated_data.dict(), f"{prefix}/{uid}")
-    except:
+    except Exception as e:
         LOGGER.error(f"[{log_tag}] Failed to store SBOM: {e}")
-    
+
+
 def main() -> None:
     """
     Script entrypoint.
     """
-    parser = argparse.ArgumentParser(description="Store SBOM Regeneration data to S3 bucket",)
-    parser.add_argument("--type", choices=["snapshots","release-data"], required=True, help="Type of SBOM data")
+    parser = argparse.ArgumentParser(
+        description="Store SBOM Regeneration data to S3 bucket",
+    )
+    parser.add_argument(
+        "--type",
+        choices=["snapshots", "release-data"],
+        required=True,
+        help="Type of SBOM data",
+    )
     parser.add_argument("--input", required=True, help="Path to the JSON input file")
     parser.add_argument("--uid", required=True, help="Tekton Pipeline run UID")
     parser.add_argument("--bucket", required=True, help="The name of the S3 bucket")
     args = parser.parse_args()
 
     try:
-        with open(args.input, "r") as sbom_file:
+        with open(args.input) as sbom_file:
             sbom_data = json.load(sbom_file)
     except Exception as e:
         LOGGER.error(f"Failed to read input file: {e}")
         raise
 
-    bucket = BucketClient(args.bucket)
+    BucketClient(args.bucket)
 
     config = {
         "snapshots": (Snapshot, "SNAPSHOT"),
-        "release-data": (ReleaseData, "RELEASE_DATA")
+        "release-data": (ReleaseData, "RELEASE_DATA"),
     }
 
     model_class, log_tag = config[args.type]
@@ -82,4 +94,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
