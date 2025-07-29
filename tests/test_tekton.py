df6bd1a9f8c1f05aedec9d328bf714696bae5b14
haripate <haripate@redhat.com>
2025-07-29 13:37:39 -0500

    feat(ISV-6005): Fix the tests.

--- tests/test_tekton.py
+++ tests/test_tekton.py
@@ -1,17 +1,19 @@
 from pathlib import Path
-from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
 from pytest import MonkeyPatch
 
 from mobster.tekton.common import AtlasTransientError, upload_sboms
-from mobster.tekton.s3 import S3Client
 
 
 @patch("mobster.tekton.common.upload_to_s3")
+@patch("mobster.tekton.common.connect_with_s3")
 @pytest.mark.asyncio
 async def test_upload_sboms_failure_tries_s3(
-    s3_client: S3Client, mock_upload_to_s3: AsyncMock, monkeypatch: MonkeyPatch
+    mock_connect_to_s3: MagicMock,
+    mock_upload_to_s3: AsyncMock,
+    monkeypatch: MonkeyPatch,
 ) -> None:
     """
     Verify that an S3 retry is attempted when upload_to_atlas fails with a
@@ -23,8 +25,10 @@ async def test_upload_sboms_failure_tries_s3(
     monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "dummy")
     monkeypatch.setenv("AWS_ACCESS_KEY_ID", "dummy")
 
+    client = mock_connect_to_s3.return_value
+
     with patch(
         "mobster.tekton.common.upload_to_atlas", side_effect=AtlasTransientError
     ):
-        await upload_sboms(s3_client, Path("dir"), "atlas_url", "retry_bucket")
+        await upload_sboms(client, Path("dir"), "atlas_url", "retry_bucket")
         mock_upload_to_s3.assert_called_once()
