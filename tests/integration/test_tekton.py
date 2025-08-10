5f818689acfe44b6b5169efee755538bbc5bbb95
Martin Jediny <jedinym@proton.me>
2025-08-10 16:24:40 +0200

    test(ISV-6197): create "slow" pytest marker
    Signed-off-by: Martin Jediny <jedinym@proton.me>

--- tests/integration/test_tekton.py
+++ tests/integration/test_tekton.py
@@ -354,7 +354,7 @@ async def test_process_component_sboms_happypath(
 
 
 @pytest.mark.asyncio
-@pytest.mark.big_release
+@pytest.mark.slow
 async def test_process_component_sboms_big_release(
     test_id: str,
     s3_client: S3Client,
