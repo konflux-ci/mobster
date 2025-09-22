0d946059bad6e2953434f2f5605682e638185dd6
BorekZnovustvoritel <mszymutk@redhat.com>
2025-09-22 10:55:39 +0200

    fix(ISV-5818): Address concurrency issues with cosign - try to make it a teeny bit faster.

--- src/mobster/oci/cosign.py
+++ src/mobster/oci/cosign.py
@@ -109,7 +109,7 @@ class CosignClient(Cosign):
         signing_key: Path | str | None = None,
         signing_key_password: bytes = b"",
         rekor_config: RekorConfig | None = None,
-        concurrency: int = 4,
+        concurrency: int = 6,
     ) -> None:
         """
         Args:
