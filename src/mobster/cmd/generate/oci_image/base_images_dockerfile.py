38d6f776a3a13a5d18390aa61bfdf564c133010f
Brian Lindner <blindner@redhat.com>
2025-10-21 16:20:24 -0400

    fix(ISV-6383): BUILD_TOOL_OF/DESCENDANT_OF fixes
    previously the app would set a `DESCENDANT_OF` relation on the last
    builder image as the base when building a container from scratch; this
    now properly adds that builder image as a BUILD_TOOL_OF

--- src/mobster/cmd/generate/oci_image/base_images_dockerfile.py
+++ src/mobster/cmd/generate/oci_image/base_images_dockerfile.py
@@ -360,14 +360,24 @@ async def _extend_spdx_with_base_images(
                     related_spdx_element_id=root_spdxid,
                 )
             )
-        # Handle Parent image
-        sbom.relationships.append(
-            Relationship(
-                spdx_element_id=root_spdxid,
-                relationship_type=RelationshipType.DESCENDANT_OF,
-                related_spdx_element_id=packages[-1].spdx_id,
+        # If building from scratch, add the last package as a build tool
+        if base_image_refs[-1] is None:
+            sbom.relationships.append(
+                Relationship(
+                    spdx_element_id=packages[-1].spdx_id,
+                    relationship_type=RelationshipType.BUILD_TOOL_OF,
+                    related_spdx_element_id=root_spdxid,
+                )
             )
-        )
+        else:
+        # Otherwise, relate this image as a descendant of the last package
+            sbom.relationships.append(
+                Relationship(
+                    spdx_element_id=root_spdxid,
+                    relationship_type=RelationshipType.DESCENDANT_OF,
+                    related_spdx_element_id=packages[-1].spdx_id,
+                )
+            )
 
 
 async def _extend_cdx_with_base_images(
