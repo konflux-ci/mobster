c8684513bb39d8a96b9c17f402fe51a0c8b952f7
mvaras <maario.vrs@gmail.com>
2026-03-06 10:26:17 +0100

    feat(ISV-6789): add support for custom CA bundles in TPA operations

--- src/mobster/utils.py
+++ src/mobster/utils.py
@@ -119,3 +119,18 @@ async def load_sbom_from_json(file_path: Path) -> dict[str, Any]:
             )
             LOGGER.critical(contents[:200])
             raise
+
+
+def get_tpa_ca() -> str | None:
+    """
+    Helper function to know if MOBSTER_TPA_CA_INFO env var is defined
+
+    Returns:
+        MOBSTER_TPA_CA_INFO value or None if not defined
+    """
+
+    return (
+        os.environ["MOBSTER_TPA_CA_INFO"]
+        if "MOBSTER_TPA_CA_INFO" in os.environ
+        else None
+    )
